03fb94cbb3941b189988009ea0012c3f985dd1b7
Justin <jcdiner@gmail.com>
2023-03-27 16:58:16 -0400

    routes

--- pokedex-rails-backend/config/routes.rb
+++ pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+	namespace :api, defaults: { format: :json } do
+		resources :pokemon do 
+			resources :items, only: [:index, :create]
+		end
+		resources :items, only: [:update, :destroy]
+	end
 end
