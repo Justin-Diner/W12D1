67cf9662adba104aaaf66ea60989783788d81223
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-27 15:29:04 -0400

    associations done

--- pokedex-rails-backend/app/models/pokemon.rb
+++ pokedex-rails-backend/app/models/pokemon.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+
+    has_many{
+        :poke_moves,
+        primary_key: :id,
+        foreign_key: :pokemon_id,
+        class_name: :PokeMove,
+        dependent: :destroy
+    }
+
+    has_many{
+        :moves,
+        through: :poke_moves,
+        source: :move,
+        dependent: :destroy
+    }
+
+
 end
